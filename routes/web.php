227fdf620da5c0ca8a1588824331d550899dabc9
Renan Catelan <53977749+renancatelan@users.noreply.github.com>
2024-06-26 16:42:56 -0300

    Laravel 11 Login controle de session

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +20,13 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+//route admin
+Route::get('admin/dashboard', [AdminController::class, 'dashboard'])
+->middleware(['auth','admin'])
+->name('admin.dashboard');
+
+//route vendor
+Route::get('vendor/dashboard', [VendorController::class, 'dashboard'])
+->middleware(['auth','vendor'])
+->name('vendor.dashboard');
